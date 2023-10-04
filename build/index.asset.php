d427092f8b107bd4cdca6edd2cbf853c381916a1
Will Sides <32076464+willsides@users.noreply.github.com>
2023-10-03 22:07:38 -0700

    initial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '0e73c960a272f59d4869');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3f82c017a93ad805e1ca');
